e799103d46338443138e88100943f5cf567f0ffd
Anurag-kuche <anuragkuche1@gmail.com>
2025-03-18 17:01:27 +0530

    added navbar

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-
 const NavbarLogo = () => {
-  return (
-    <div>
-      ooo
-    </div>
-  )
-}
-
-export default NavbarLogo
+    return (
+      <div>
+        <h1 className="text-white  text-2xl sm:hidden md:block">
+          Anurag Kuche
+        </h1>
+        <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
+          AK
+        </h1>
+      </div>
+    );
+  };
+  
+  export default NavbarLogo;
