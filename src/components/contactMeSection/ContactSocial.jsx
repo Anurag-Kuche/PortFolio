5b8ad55ceaa5898f2454c90e5d7150a0f8b92343
Anurag-kuche <anuragkuche1@gmail.com>
2025-07-15 00:04:19 +0530

    new commit

--- src/components/contactMeSection/ContactSocial.jsx
+++ src/components/contactMeSection/ContactSocial.jsx
@@ -6,8 +6,8 @@ import { FaInstagram } from "react-icons/fa";
 const ContactSocial = () => {
   return (
     <div className="flex gap-4">
-      <SingleContactSocial link="#" Icon={FaLinkedinIn} />
-      <SingleContactSocial link="#" Icon={FiGithub} />
+      <SingleContactSocial link="https://www.linkedin.com/in/anurag-kuche-17843a28a/" Icon={FaLinkedinIn} />
+      <SingleContactSocial link="https://github.com/Anurag-Kuche" Icon={FiGithub} />
       <SingleContactSocial link="#" Icon={FaInstagram} />
     </div>
   );
