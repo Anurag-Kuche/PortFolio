ebae26398b94f0974effce11faf77add77fca0c1
Anurag Kuche <anuragkuche1@gmail.com>
2025-07-15 18:21:33 +0530

    Update NavbarLogo.jsx

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -1,14 +1,21 @@
+"use client";
+import React from "react";
+
 const NavbarLogo = () => {
-    return (
-      <div>
-        <h1 className="text-white  text-2xl sm:hidden md:block">
-          𝑨𝒏𝒖𝒓𝒂𝒈𝑲𝒖𝒄𝒉𝒆.dev
-        </h1>
-        <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
-          AK
-        </h1>
-      </div>
-    );
+  const handleClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
-  
-  export default NavbarLogo;
+
+  return (
+    <div onClick={handleClick} className="cursor-pointer">
+      <h1 className="text-white text-2xl sm:hidden md:block">
+        𝑨𝒏𝒖𝒓𝒂𝒈𝑲𝒖𝒄𝒉𝒆.dev
+      </h1>
+      <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
+        AK
+      </h1>
+    </div>
+  );
+};
+
+export default NavbarLogo;
