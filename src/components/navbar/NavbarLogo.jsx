c31d9ba1fd31d56ccbe9bd26701b5e05bfaaf09c
Anurag Kuche <anuragkuche1@gmail.com>
2025-07-15 18:19:39 +0530

    Update NavbarLogo.jsx

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -2,7 +2,7 @@ const NavbarLogo = () => {
     return (
       <div>
         <h1 className="text-white  text-2xl sm:hidden md:block">
-          𝑨𝒏𝒖𝒓𝒂𝒈 𝑲𝒖𝒄𝒉𝒆.dev
+          𝑨𝒏𝒖𝒓𝒂𝒈𝑲𝒖𝒄𝒉𝒆.dev
         </h1>
         <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
           AK
